b63257164c2569d00e30382f4eff99a29bc8ff82
Ryan Howe <rhowe@truecar.com>
2020-03-17 16:48:36 -0400

    Added node-cache to project and env vars
    This is starting to utilize the env vars that will be set on the server
    to access keys.  Still have not finalized an implementation strategy for
    the server and service

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var NodeCache = require('node-cache');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -19,8 +19,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+const myCache = new NodeCache({checkperiod: 300});
+
+
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+//app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,13 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'theAxeRant' });
 });
 
+/* GET AccuWeather */
+router.post('/api', function(req, res, next){
+  if(process.env.THEAXERANT_API_KEY !== req.params.api_key) { 
+    res.send(process.env.THEAXERANT_API_KEY + '</br>Weather</br>' + req.params.api_key);
+  }
+  res.send('No!').status(500); 
+  
+});
+
 module.exports = router;
